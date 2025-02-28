6a4ebe96812931e8fb1ed992ae62d80af4af5095
Theo Cattaneo <theo.cattaneo.etu@univ-lille.fr>
2025-02-28 11:22:07 +0100

    change pour application de base

--- app.py
+++ app.py
@@ -1,32 +1,14 @@
-from flask import Flask, render_template, request, redirect, url_for
-import sqlite3
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
-DATABASE = 'notes.db'
-
-def get_db_connection():
-    conn = sqlite3.connect(DATABASE)
-    conn.row_factory = sqlite3.Row
-    return conn
 
 @app.route('/')
 def index():
-    conn = get_db_connection()
-    notes = conn.execute('SELECT * FROM notes').fetchall()
-    conn.close()
-    return render_template('index.html', notes=notes)
+    return redirect(url_for('display_var'))
 
-@app.route('/add', methods=('GET', 'POST'))
-def add_note():
-    if request.method == 'POST':
-        title = request.form['title']
-        content = request.form['content']
-        conn = get_db_connection()
-        conn.execute('INSERT INTO notes (title, content) VALUES (?, ?)', (title, content))
-        conn.commit()
-        conn.close()
-        return redirect(url_for('index'))
-    return render_template('add_note.html')
+@app.route('/app')
+def display_var():
+    return render_template('display_var.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
